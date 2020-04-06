755cd3b8fa6790d3c0c54b4ff0514b6ee58ee19e
Diego Caliri <diegocaliri@gmail.com>
2020-04-05 23:29:44 -0300

    Bootstrap 4 using webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,7 @@ require("chartkick")
 require("chart.js")
 
 import '../stylesheets/application'
+import './bootstrap_custom.js'
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
