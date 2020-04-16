10df6ee8008e75ffd62af45afd6ffceefbc76836
Diego Caliri <diegocaliri@gmail.com>
2020-04-16 18:24:15 -0300

    Navegación, filtros, vistas para muertes y casos activos

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,35 @@
 class HomeController < ApplicationController
 
   before_action :get_data
+  before_action :check_params
 
   def index
-    @countries = @json.map{ |x| x.first }
+  end
+
+  def deaths
+  end
+
+  def active
   end
 
   def json_series
-    @json = @json.select{|x| params[:countries].split(',').include?(x)} if params[:countries].present?
-    @json = @json.map{|k,v| [k, @json[k].select{ |x| x if x['date'].to_date >= params[:since].to_date }]}.to_h if params[:since].present?
-    @countries = @json.map{|x| [ x[0], x[1].map{|y| [ y['date'], y['confirmed'] ] }.to_h ] }
-    json = @countries.map{|x| { name: x[0], data: x[1] } }
+    @json = @json.select{|x| @countries.split(',').include?(x)}
+    @json = @json.map{|k,v| [k, @json[k].select{ |x| x if x['date'].to_date >= @date.to_date }]}.to_h
+    case params[:info]
+    when 'infected'
+      @data = @json.map{|x| [ x[0], x[1].map{|y| [ y['date'], y['confirmed'] ] }.to_h ] }
+    when 'deaths'
+      @data = @json.map{|x| [ x[0], x[1].map{|y| [ y['date'], y['deaths'] ] }.to_h ] }
+    when 'active'
+      @data = @json.map{|x| [ x[0], x[1].map{|y| [ y['date'], y['confirmed'] - y['deaths'] - y['recovered'] ] }.to_h ] }
+    end
+    json = @data.map{|x| { name: x[0], data: x[1] } }
     render json: json
+  end
+
+  def check_params
+    @date = params[:since].present? ? params[:since] : '01/03/2020'
+    @countries = params[:countries].present? ? params[:countries] : 'Argentina,Chile,Brazil'
   end
 
   def get_data
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   get 'json_series', to: 'home#json_series'
+  get 'deaths', to: 'home#deaths'
+  get 'active', to: 'home#active'
 end
